f563aa0a68f1f3ee0409eb2f23b9ca7064bc2a85
alireza <alirezadarvishi.me@gmail.com>
2022-02-10 10:12:41 +0330

    Add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,35 @@
-import express, { Express } from 'express';
+import express, { Express } from "express";
 
-import { NODE_ENV, PORT } from './config/index';
-import apiV1Routes from './routes/api';
+import { NODE_ENV, PORT } from "./config/index";
+import apiV1Routes from "./routes/api";
 
 class Application {
-	app: Express;
+  app: Express;
 
-	constructor() {
-		this.app = express();
-		this.middlewares();
-		this.routes();
-	}
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
 
-	private middlewares() {
-		this.app.use(express.urlencoded({ extended: false }));
-		this.app.use(express.json());
-	}
+  private middlewares() {
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+  }
 
-	private routes() {
-		this.app.use('/api/v1', apiV1Routes);
-	}
+  private routes() {
+    this.app.use("/api/v1", apiV1Routes);
+  }
 
-	public run(port?: number | string, callback?: () => void) {
-		const APPPORT = port || PORT;
+  public run(port?: number | string, callback?: () => void) {
+    const APPPORT = port || PORT;
 
-		if (callback) return this.app.listen(APPPORT, callback);
+    if (callback) return this.app.listen(APPPORT, callback);
 
-		this.app.listen(APPPORT, () => {
-			console.log(`Server is running on port ${APPPORT} and ${NODE_ENV} mode`);
-		});
-	}
+    this.app.listen(APPPORT, () => {
+      console.log(`Server is running on port ${APPPORT} and ${NODE_ENV} mode`);
+    });
+  }
 }
 
 export default Application;
