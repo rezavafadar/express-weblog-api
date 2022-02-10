b846e0a538d3c4356237499cebaacab4da2f7d82
alireza <alirezadarvishi.me@gmail.com>
2022-02-10 09:46:03 +0330

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensivity: "css",
+  insertPragma: false,
+  bracketSameLine: false,
+  proseWrap: "always",
+  requirePragma: false,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
