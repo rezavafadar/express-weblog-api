7f7a64192b42d2ae77fc70cbc9fdc044db8d230d
rezavr <mrvafadar4@gmail.com>
2022-02-12 18:07:30 +0330

    change user service

--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -1,18 +1,1 @@
-import { AppError } from '../error/AppError';
-import prisma from '../../../prisma/prisma-client';
-
-export class UserService {
-	async checkUserUniques(username: string = '', email: string = '') {
-		const user = await prisma.user.findUnique({
-			where: {
-				email,
-				username,
-			},
-			select: {
-				id: true,
-			},
-		});
-
-		if (user) throw new AppError('failed!',403,'Already taken',true);
-	}
-}
+/** comming soon */
