40e5b00f4cded115853bc2d2215aa837ecdb2551
rezavr <mrvafadar4@gmail.com>
2022-02-12 18:05:02 +0330

    change structure and validation layer

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -1,9 +1,26 @@
-import type { Request, Response } from "express";
+import { AppError } from '../common/appError';
+import { AuthService } from './auth.service';
+import type { Request, Response } from 'express';
+import authValidate from './auth.validation';
 
-const register = async (req: Request, res: Response) => {
-  await res.json({ ok: true });
-};
+class AuthController {
+  private readonly authService: AuthService;
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
 
-export default {
-  register,
-};
+  async register(req: Request, res: Response) {
+    const userValidation = await authValidate(req.body);
+
+    if (userValidation !== null)
+      throw new AppError('Validation', 400, true, userValidation.message);
+
+    await this.authService.registerUser(req.body);
+
+    res.status(201).json({
+      message: 'successfull',
+    });
+  }
+}
+
+export default AuthController;
