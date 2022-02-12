541890724b7e2daa967bef82ddf73f8dce622726
rezavr <mrvafadar4@gmail.com>
2022-02-12 18:13:54 +0330

    use connect db func

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config({ path: `src/config/.${process.env.NODE_ENV}.env` });
 
-import Application from './app';
+import Application from "./app";
+import connectToDb from "./database/connectDB";
 
 const app = new Application();
+
+connectToDb();
 
 app.run();
