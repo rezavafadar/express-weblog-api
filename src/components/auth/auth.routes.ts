c031e93990261b925071dff88fb5e5668cbdf13f
rezavr <mrvafadar4@gmail.com>
2022-02-12 18:05:46 +0330

    create routes controller

--- src/components/auth/auth.routes.ts
+++ src/components/auth/auth.routes.ts
@@ -1,12 +1,24 @@
-import authController from "./auth.controller";
-import { Router } from "express";
+import { Router } from 'express';
+
+import AuthController from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthDal } from './auth.DAL';
+import wrapper from '../common/wrapper';
+import AuthEmail from './auth.email';
+import { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS } from '../../config';
+
+const authEmail = new AuthEmail({
+  host: EMAIL_HOST,
+  port: Number(EMAIL_PORT),
+  auth: { user: EMAIL_USER, pass: EMAIL_PASS },
+});
+
+const authController = new AuthController(
+  new AuthService(new AuthDal(), authEmail),
+);
 
 const authRouter = Router();
 
-authRouter.get("/", () => {
-  console.log("Get the auth route");
-});
-
-authRouter.post("/register", authController.register);
+authRouter.post('/register', wrapper(authController.register));
 
 export default authRouter;
