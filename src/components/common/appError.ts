749a33f10d5931d06d5c1ecb0a9db9b65548f9b8
rezavr <mrvafadar4@gmail.com>
2022-02-12 18:13:17 +0330

    create common component and move common files to it

---
+++ src/components/common/appError.ts
@@ -0,0 +1,22 @@
+export class AppError extends Error {
+  public readonly name: string;
+  public readonly httpCode: number;
+  public readonly isOperational: boolean;
+
+  constructor(
+    name: string,
+    httpCode: number,
+    isOperational: boolean,
+    description: string
+  ) {
+    super(description);
+
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+
+    this.name = name;
+    this.httpCode = httpCode;
+    this.isOperational = isOperational;
+
+    Error.captureStackTrace(this);
+  }
+}
---
+++ src/components/common/wrapper.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const wrapper =
+  (fn: (req: Request, res: Response, next: NextFunction) => any) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+
+export default wrapper;
