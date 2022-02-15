40439d38a5a43fdf48e21635afd8c74d30730e38
rezavr <mrvafadar4@gmail.com>
2022-02-16 00:03:44 +0330

    add verify user account service

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -1,13 +1,52 @@
+import { CreateVerifyPayload } from './../../interfaces/auth.interfaces';
 import { AppError } from './../common/appError';
 import { CreateUserPayload } from './../../interfaces/user.interfaces';
-import { AuthDal } from './auth.DAL';
-import AuthEmail from './auth.email';
+import type { AuthDal } from './auth.DAL';
+import type AuthEmail from './auth.email';
+import { randomStringGenerator } from '../../utils/randomStr';
 
 export class AuthService {
   constructor(
     private readonly authDal: AuthDal,
     private readonly authEmail: AuthEmail,
   ) {}
+
+  async verifyEmail(email: string): Promise<any> {
+    return this.authDal.transaction(async (prisma) => {
+      const checkEmail = await this.authDal.checkVerifyEmail(email);
+
+      const code = randomStringGenerator(4);
+      const verifyData: CreateVerifyPayload = {
+        email,
+        confirm_code: code,
+        time_expire: new Date(Date.now() + 2 * 60 * 1000),
+      };
+      console.log(checkEmail);
+
+      if (checkEmail == null) {
+        await this.authDal.saveVerifyEmail(verifyData, prisma);
+      } else {
+        if (checkEmail && checkEmail.verify === true)
+          throw new AppError(
+            'Verify failed!',
+            403,
+            true,
+            'Email is already verify!',
+          );
+
+        if (Date.now() < +checkEmail.time_expire)
+          throw new AppError(
+            'Time expire error!',
+            401,
+            true,
+            'Please act later!',
+          );
+
+        await this.authDal.editVerifyCode(verifyData, prisma);
+      }
+      await this.authEmail.verifyEmail(email, code);
+    });
+  }
 
   async registerUser(input: CreateUserPayload): Promise<void> {
     const checkUser = await this.authDal.checkUserUniques(
@@ -23,8 +62,6 @@ export class AuthService {
         'Already taken',
       );
 
-    await this.authDal.storeUser(input);
-
-    await this.authEmail.verifyEmail(input.email, '1234');
+    await this.authDal.saveUser(input);
   }
 }
