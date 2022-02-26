e3cd17e98e050bd47bf34163932233e13a875377
rezavr <mrvafadar4@gmail.com>
2022-02-26 23:16:20 +0330

    refactoring

--- src/utils/bcrypt.ts
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const hashData = (data:string) => bcrypt.hash(data,10)
-
-export const compareHash = (data:string,encryptedData:string) => bcrypt.compare(data,encryptedData)
