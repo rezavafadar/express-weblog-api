fe95128d45cb2a06c4db1ca09cfef32b093b0453
rezavr <mrvafadar4@gmail.com>
2022-02-27 18:05:06 +0330

    move error handler to exception folder and refactoring is

--- src/exception/errorHandler.ts
+++ src/exception/errorHandler.ts
@@ -3,7 +3,7 @@ import type { NextFunction, Request, Response } from 'express';
 const wrapper =
   (fn: (req: Request, res: Response, next: NextFunction) => any) =>
   (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
 
 export default wrapper;
