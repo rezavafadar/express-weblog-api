72e23492fac78d236fb24d2b5f8618671e9b3d99
rezavr <mrvafadar4@gmail.com>
2022-02-27 23:03:29 +0330

    add verify handler to auth service

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -2,23 +2,55 @@ import type { AuthServicePayload } from '../../interfaces/services.interfaces';
 import type EmailSender from '../../services/email/emailSender';
 import type AuthDal from './auth.DAL';
 
+import authValidators from './auth.validate';
 import { ExceptionError } from '../../exception/exceptionError';
+import { randomCodeGenerator } from '../../utils/randomCode';
 
 export class AuthService implements AuthServicePayload {
   constructor(private authDal: AuthDal, private emailService: EmailSender) {}
 
-  async verify(email: string) {
-    const user = await this.authDal.getActivateUser(email);
+  async userExistence(type: 'login' | 'register', email: string) {
+    await authValidators.existenceUserValidation({ type, email });
 
-    if (user && user.active) throw new ExceptionError('422', 422, '');
-  }
-
-  async userExistence(type: string, email: string) {
     const { active } = await this.authDal.getActivateUser(email);
     if (type === 'login' && active !== true)
       throw new ExceptionError('User exists', 422, 'User is Not Exists!');
     if (type === 'register' && active === true)
       throw new ExceptionError('User exists', 422, 'User is Exists!');
+  }
+
+  async verify(email: string) {
+    await authValidators.verifyValidation(email);
+
+    let user = await this.authDal.getActivateUser(email);
+
+    if (user && user.active)
+      throw new ExceptionError(
+        'Unauthorized',
+        401,
+        'A user with this profile exists',
+      );
+
+    const isCodeExists = await this.authDal.getCodeByEmail(user.email);
+
+    if (isCodeExists)
+      throw new ExceptionError(
+        'Code Exists!',
+        401,
+        'Your code is not expire! Please try again later.',
+      );
+
+    const code = randomCodeGenerator(5);
+
+    await this.authDal.setCodeByEmail(email, code);
+
+    if (!user) user = await this.authDal.createUser(email);
+
+    this.emailService.sendVerifyCode(user.email, code).catch((e) => {
+      console.log('Email Error: ', e);
+    });
+
+    return user;
   }
 }
 
--- src/interfaces/services.interfaces.ts
+++ src/interfaces/services.interfaces.ts
@@ -1,4 +1,5 @@
+import { User } from './../schema/user.schema';
 export interface AuthServicePayload {
-  verify: (email: string) => Promise<void>;
+  verify: (email: string) => Promise<User>;
   userExistence: (type: string, email: string) => Promise<void>;
 }
