a8cada989ebf37fc841b7abef0b52a3a14213e03
rezavr <mrvafadar4@gmail.com>
2022-02-27 13:20:04 +0330

    add check user existence handler method

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -1,10 +1,19 @@
-import EmailSender from '../../services/email/emailSender';
 import type { AuthServicePayload } from '../../interfaces/services.interfaces';
-import AuthDal from './auth.DAL';
+import type EmailSender from '../../services/email/emailSender';
+import type AuthDal from './auth.DAL';
+
+import { AppError } from '../../exception/appError';
 
 export class AuthService implements AuthServicePayload {
   constructor(private authDal: AuthDal, private emailService: EmailSender) {}
-  verify() {}
+  async verify() {}
+  async userExistence(type: string, email: string) {
+    const { active } = await this.authDal.getActivateUser(email);
+    if (type === 'login' && active !== true)
+      throw new AppError('User exists', 422, 'User is Not Exists!');
+    if (type === 'register' && active === true)
+      throw new AppError('User exists', 422, 'User is Exists!');
+  }
 }
 
 export default AuthService;
