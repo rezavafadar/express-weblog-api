d4b37e9ff3f6daefbb9c247e23222c8adce58ca4
rezavr <mrvafadar4@gmail.com>
2022-02-27 22:39:50 +0330

    use emailer class and config

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -1,10 +1,28 @@
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+
 import AuthDal from './auth.DAL';
 import AuthController from './auth.controller';
 import AuthService from './auth.service';
 import EmailSender from '../../services/email/emailSender';
+import {
+  EMAIL_HOST,
+  EMAIL_PASS,
+  EMAIL_PORT,
+  EMAIL_USER,
+} from '../../config/index';
+
+const mailerOption: SMTPTransport.Options = {
+  host: EMAIL_HOST,
+  port: +EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
+  },
+};
 
 const authController = new AuthController(
-  new AuthService(new AuthDal(), new EmailSender()),
+  new AuthService(new AuthDal(), new EmailSender(mailerOption)),
 );
 
 export default authController;
