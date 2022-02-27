9e9e2130b8632abf9547561b6606192dad1fb1a6
rezavr <mrvafadar4@gmail.com>
2022-02-27 23:01:12 +0330

    init auth dal and add getActivateUser & createUser & getCodeByEmail & setCodeByEmail

--- src/components/auth/auth.DAL.ts
+++ src/components/auth/auth.DAL.ts
@@ -1,4 +1,7 @@
+import type { CreateUserPayload } from '../../schema/user.schema';
+
 import prisma from '../../database/prisma-client';
+import redisClient from '../../database/redis-client';
 
 class AuthDal {
   getActivateUser(email: string) {
@@ -6,10 +9,25 @@ class AuthDal {
       where: {
         email,
       },
-      select: {
-        active: true,
-      },
     });
+  }
+
+  async createUser(email: string) {
+    const user: CreateUserPayload = {
+      email,
+      username: email.split('@')[0] + '_U',
+    };
+    return prisma.user.create({
+      data: user,
+    });
+  }
+
+  getCodeByEmail(email: string) {
+    return redisClient.get(email);
+  }
+
+  setCodeByEmail(email: string, code: string) {
+    return redisClient.setEx(email, 300, code);
   }
 }
 
