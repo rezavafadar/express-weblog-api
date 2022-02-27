4ad4e219d6346347a28d6f3aaa9f51da94cb2a42
rezavr <mrvafadar4@gmail.com>
2022-02-27 18:08:17 +0330

    add validation layer to user existence method

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -1,15 +1,26 @@
 import type { Request, Response } from 'express';
-
-import { Controller, Post } from '../../decorators/routing.decorator';
 import type { AuthServicePayload } from '../../interfaces/services.interfaces';
+
+import { Controller, Get, Post } from '../../decorators/routing.decorator';
+import authValidators from './auth.validate';
 
 @Controller('/auth')
 class AuthController {
   constructor(private readonly authService: AuthServicePayload) {}
 
+  @Get('/exists-user')
+  async checkExistenceUser(req: Request, res: Response) {
+    const body = req.body;
+
+    await authValidators.existenceUserValidation(body);
+
+    await this.authService.userExistence(body.type, body.email);
+
+    res.status(200).json({ message: 'Successful!' });
+  }
+
   @Post('/verify')
   async verify(req: Request, res: Response) {
-    console.log(req.body);
     res.status(400).json({ msg: 'ok' });
   }
 }
