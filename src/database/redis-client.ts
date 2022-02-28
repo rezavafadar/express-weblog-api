788601b7236f6eeeb40d648d0be4ee5fa0126320
rezavr <mrvafadar4@gmail.com>
2022-02-28 16:05:38 +0330

    add redis client error handler to redis client

--- src/database/redis-client.ts
+++ src/database/redis-client.ts
@@ -2,5 +2,8 @@ import { createClient } from 'redis';
 import { REDIS_URL } from '../config';
 
 const redisClient = createClient({ url: REDIS_URL });
-
+redisClient.on('error', (err) => {
+  console.error('Redis Error: ', err);
+  process.exit(1);
+});
 export default redisClient;
