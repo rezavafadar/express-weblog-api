59a78e6fab46af5a9862f000f3a09f62bd468b81
rezavr <mrvafadar4@gmail.com>
2022-02-28 16:47:52 +0330

    create verifycode route and controller

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import type { Request, Response } from 'express';
 import type { AuthServicePayload } from '../../interfaces/services.interfaces';
 
 import { Controller, Get, Post } from '../../decorators/routing.decorator';
+import JwtService from '../../services/jwt/jwtservice';
 
 @Controller('/auth')
 class AuthController {
-  constructor(private readonly authService: AuthServicePayload) {}
+  constructor(
+    private readonly authService: AuthServicePayload,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get('/exists-user')
   async checkExistenceUser(req: Request, res: Response) {
@@ -28,6 +32,17 @@ class AuthController {
         user_email: result.email,
       },
     });
+  }
+
+  @Post('/verify/code')
+  async verifyCode(req: Request, res: Response) {
+    const body = req.body;
+
+    const result = await this.authService.verifyCode(body.email, body.code);
+
+    const token = this.jwtService.signUserToken(result.id);
+
+    res.status(200).json({ message: 'Successfull!', data: result, token });
   }
 }
 
