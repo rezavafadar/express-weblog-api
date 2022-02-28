8cc4bb17c5621b6c6fa7a630cf41f40b2ee95b81
rezavr <mrvafadar4@gmail.com>
2022-02-28 16:47:20 +0330

    add verifyCode type to authservice interface

--- src/interfaces/services.interfaces.ts
+++ src/interfaces/services.interfaces.ts
@@ -1,5 +1,6 @@
 import { User } from './../schema/user.schema';
 export interface AuthServicePayload {
-  verify: (email: string) => Promise<User>;
   userExistence: (type: string, email: string) => Promise<void>;
+  verify: (email: string) => Promise<User>;
+  verifyCode: (email: string, code: string) => Promise<User>;
 }
