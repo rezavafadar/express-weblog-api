a9d59deddacd37ea7e4088b3b25555d7dcf5d875
rezavafadar <mrvafadar4@gmail.com>
2022-03-23 14:37:38 +0430

    add Job Queues process handler to server

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ dotenv.config({ path: `src/config/.${process.env.NODE_ENV}.env` });
 
 import Application from './app';
 import dbConnection from './database/connectDB';
+import Jobs from './jobs';
 
 const app = new Application();
 
 dbConnection.connectMySql();
 dbConnection.connectRedis();
+Jobs.startProcess();
 
 app.run();
