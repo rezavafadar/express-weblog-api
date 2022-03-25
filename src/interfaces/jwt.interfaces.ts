bb11afc3b0f534ccdbef642c8752bac228911b88
rezavafadar <mrvafadar4@gmail.com>
2022-03-25 20:31:18 +0430

    add verify and verify login tokens mwthod in jwt service

--- src/interfaces/jwt.interfaces.ts
+++ src/interfaces/jwt.interfaces.ts
@@ -3,4 +3,5 @@ import { userRole } from '../schema/user.schema';
 export interface SignUserPayload {
   role: userRole;
   id: number;
+  ext: number;
 }
--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,11 +1,33 @@
 import { IJwtService } from '../interfaces/services.interfaces';
 import { SignUserPayload } from '../interfaces/jwt.interfaces';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config';
+import { ExceptionError } from '../exception/exceptionError';
 
 class JwtService implements IJwtService {
+  private secretKey: string;
+  constructor(secretKey: string) {
+    this.secretKey = secretKey;
+  }
+
   signUserToken(payload: SignUserPayload) {
-    return jwt.sign(payload, JWT_SECRET);
+    return jwt.sign(payload, this.secretKey);
+  }
+
+  verifyToken(token: string) {
+    try {
+      const payload = jwt.verify(token, this.secretKey);
+      return payload;
+    } catch (error) {
+      console.log(error);
+      throw new ExceptionError('Bad Request!', 400, 'Token is invalid!');
+    }
+  }
+
+  verifyLoginToken(token: string) {
+    const verifyToken: any = this.verifyToken(token);
+    if (!verifyToken.id)
+      new ExceptionError('Bad Request!', 400, 'Token is invalid!');
+    return verifyToken;
   }
 }
 
